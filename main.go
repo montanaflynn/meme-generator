5cdf9ba9c690033e8a47bfea39ba3c3051798188
Montana Flynn <montana@montanaflynn.me>
2016-02-23 16:36:44 -0800

    Download meme templates and add list of memes option

--- main.go
+++ main.go
@@ -6,53 +6,67 @@ import (
 	"image"
 	_ "image/jpeg"
 	"log"
+	"net/http"
 	"os"
+
+	_ "./vendor/statik"
 
 	"github.com/fogleman/gg"
 )
 
-const fontSize = 36
+func LoadImage(file string) image.Image {
 
-func LoadImage(filename string) image.Image {
-	file, err := os.Open(filename)
+	url := fmt.Sprintf("https://imgflip.com/s/meme/%s", file)
+	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("%s template from %s failed because of %v", file, url, err)
 	}
-	defer file.Close()
-	image, something, err := image.Decode(file)
+	defer res.Body.Close()
+	image, _, err := image.Decode(res.Body)
 	if err != nil {
-		log.Fatal(something, err)
+		log.Fatalf("Could not decode %s because of %v", file, err)
 	}
 	return image
 }
 
 func main() {
+	const fontSize = 36
+
 	meme := flag.String("meme", "fry", "the meme to use")
 	text := flag.String("text", "not sure what to put here", "the text to use")
+	list := flag.Bool("list", false, "list of available memes")
 	flag.Parse()
-
-	tail := flag.Args()
-	if len(tail) == 0 {
-		flag.Usage()
-	}
-
-	path := tail[0]
 
 	memes := make(map[string]string)
 	memes["fry"] = "Futurama-Fry.jpg"
 	memes["aliens"] = "Ancient-Aliens.jpg"
 	memes["doge"] = "Doge.jpg"
+	memes["simply"] = "One-Does-Not-Simply.jpg"
 
-	img := LoadImage("./media/" + memes[*meme])
+	if *list {
+		fmt.Println("Available memes:")
+		for k, _ := range memes {
+			fmt.Println(k)
+		}
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+	}
+
+	path := args[0]
+
+	img := LoadImage(memes[*meme])
 	r := img.Bounds()
 	w := r.Dx()
 	h := r.Dy()
 
 	m := gg.NewContext(w, h)
 	m.DrawImage(img, 0, 0)
-	m.SetRGB255(255, 255, 255)
-	m.SetLineWidth(10)
 	m.LoadFontFace("/Library/Fonts/Impact.ttf", fontSize)
+	m.SetRGB255(255, 255, 255)
 	m.DrawStringAnchored(*text, float64(w)/2, float64(h)-fontSize, .5, .5)
 	m.SavePNG(path)
 	fmt.Printf("Saved to %s\n", path)
